8f58d1c1e001abb393fb6abcc0240afba07169eb
accessibleStats <95653385+accessibleStats@users.noreply.github.com>
2021-12-10 20:26:43 -0600

    Add files via upload

--- arithmeticprogram.py
+++ arithmeticprogram.py
@@ -6,46 +6,45 @@ Multiplication, Division, Addition, and Subtraction Program - with simple input 
 By: Jack Nickelson
 """
 
-print("This is a multiplication, division, addition, and subtraction program!")
-while True:
-    decision = str(input("Would you like to M, D, A, or S?"))
-    print("Type M for multiplication, D for division, A for addition, or S for subtraction: ")
-    try:
-        if decision == "A" or decision == "a":
-            first_number=int(input("Addend 1:"))
-            second_number=int(input("Addend 2:"))
-            sum=first_number+second_number
-            print("Enter your numbers separated by a comma")
-            print('The sum of {} and {} is: {}'.format(first_number,second_number,sum))
-            break
-        elif decision == "S" or decision == "s":
-            first_number=int(input("Minuend:"))
-            second_number=int(input("Subtrahend:"))
-            difference=first_number-second_number
-            print('The difference of {} and {} is: {}'.format(first_number,second_number,difference)) 
-            break  
-        elif decision == "M" or decision == "m":
-            first_number=int(input("Factor_1:"))
-            second_number=int(input("Factor_2:"))
-            product=first_number*second_number
-            print('The product of {} and {} is: {}'.format(first_number,second_number,product))
-            break  
-        elif decision == "D" or decision == "d":
-            first_number=int(input("Dividend:"))
-            second_number=int(input("Divisor:"))
-            quotient=first_number/second_number
-            print('The quotient of {} and {} is: {}'.format(first_number,second_number,quotient))
-            break   
-        elif decision == "q":
-            break
-        elif decision == "quit":
-            break
-        elif decision == "exit":
-            break
-        elif decision == "no":
-            break
-        elif decision == "No":
-            break
-    except:
-        print('This is not a valid selection. Please input a capital letter of M, D, A, or S:')
 
+def arithmetic_2():
+    stop = input("This is a 2 number addition, subtraction, multiplication, and division program. --------- (press any key to continue)")
+    stop2 = input("You can enter 2 numbers and find their sum, difference, product or quotient. ----------(press any key to continue)")
+    while True:
+        decision = str(input("Would you like to add, subtract, multiply, or divide; press A, S, M or D? ---------(press q to exit program)"))
+        try:
+            if decision == "A" or decision == "a":
+                first_number=int(input("Addend 1:"))
+                second_number=int(input("Addend 2:"))
+                sum=first_number+second_number
+                print('The sum of {} and {} is: {}'.format(first_number,second_number,sum))
+            elif decision == "S" or decision == "s":
+                first_number=int(input("Minuend:"))
+                second_number=int(input("Subtrahend:"))
+                difference=first_number-second_number
+                print('The difference of {} and {} is: {}'.format(first_number,second_number,difference)) 
+            elif decision == "M" or decision == "m":
+                first_number=int(input("Factor_1:"))
+                second_number=int(input("Factor_2:"))
+                product=first_number*second_number
+                print('The product of {} and {} is: {}'.format(first_number,second_number,product))
+            elif decision == "D" or decision == "d":
+                first_number=int(input("Dividend:"))
+                second_number=int(input("Divisor:"))
+                quotient=first_number/second_number
+                print('The quotient of {} and {} is: {}'.format(first_number,second_number,quotient))
+            elif decision == "q":
+                break
+            elif decision == "quit":
+                break
+            elif decision == "exit":
+                break
+            elif decision == "no":
+                break
+            elif decision == "No":
+                break
+        except:
+            print('This is not a valid selection. Please input a capital letter of M, D, A, or S:')
+            break
+
+arithmetic_2()
